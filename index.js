2e232f8dc2d88799631b59c7536f0cad92a8e1dd
bellobambo <bellobambo21@gmail.com>
2024-07-19 14:38:15 +0100

    web scrapping schedulling final

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
+const cron = require("node-cron");
 
 async function start() {
   const browser = await puppeteer.launch();
@@ -22,10 +23,22 @@ async function start() {
     }
 
     await page.click("#clickme");
+    const clickedData = await page.$eval("#data", (el) => el.textContent);
+    // console.log(clickedData);
 
     const photos = await page.$$eval("img", (imgs) => {
       return imgs.map((x) => x.src);
     });
+
+    await page.type("#ourfield", "blue");
+
+    await Promise.all([
+      page.click("#ourform button"),
+      page.waitForNavigation(),
+    ]);
+
+    const info = await page.$eval("#message", (el) => el.textContent);
+    console.log(info);
 
     for (const photo of photos) {
       const imgpage = await page.goto(photo);
@@ -38,4 +51,7 @@ async function start() {
   await browser.close();
 }
 
-start();
+// call it every 5 secs
+// setInterval(start, 5000);
+
+// cron.schedule("*/5 * * * * *", start);
